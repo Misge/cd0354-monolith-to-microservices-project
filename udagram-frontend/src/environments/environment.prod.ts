ac181bdfa7fe6b2f073c56df181925dc44119bab
Misge <tesfaymisgana@gmail.com>
2022-09-24 09:00:28 +0300

    final push

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2953b201dbd844eda7db5cef6e05556-555718164.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
